a63be96da571dc0fd598738cc3838e11367b1245
rakirs2 <srmylavarapu@microsoft.com>
2022-07-07 17:29:55 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("20kg or 15kg bar\n 1 for 20 kg; 2 for 15 kg");
+
+String bar = Console.ReadLine();
+int barInput = Int16.Parse(bar);
+
+int barWeight = 20;
+if barInput = 
+
+Console.WriteLine(bar);
+Console.WriteLine(barWeight);
